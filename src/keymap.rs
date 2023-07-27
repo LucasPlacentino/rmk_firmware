f2c89d40def137c4f8f743f526b29622d47d4416
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-07-27 12:30:31 +0200

    use k! macro in keymap.rs

--- src/keymap.rs
+++ src/keymap.rs
@@ -4,6 +4,8 @@
 mod keymap_definitions;
 use keymap_definitions::{KEY, KEYMAP};
 
+// ----- DEFINE KEYMAP HERE BELOW -----
+
 // Define constants for the size of the keymap
 const COL_SIZE: usize = 8;
 const ROW_SIZE: usize = 3;
@@ -13,51 +15,52 @@ const LAYER_COUNT: usize = 3;
 const KEYMAP_LAYOUT: KEYMAP = [
     // Layer 0
     [
-        [KEY::A, KEY::B, KEY::C, KEY::A, KEY::B, KEY::C, KEY::A, KEY::B],
-        [KEY::B, KEY::C, KEY::A, KEY::B, KEY::C, KEY::A, KEY::B, KEY::C],
-        [KEY::C, KEY::A, KEY::B, KEY::C, KEY::A, KEY::B, KEY::C, KEY::A],
+        [k!(A), k!(B), None, k!(A), k!(B), k!(C), k!(A), k!(B)],
+        [k!(B), k!(C), k!(A), k!(B), k!(C), k!(A), k!(B), k!(C)],
+        [k!(C), k!(A), k!(B), k!(C), k!(A), k!(B), k!(C), None],
     ],
     // Layer 1
     [
-        [KEY::A, KEY::B, KEY::C, KEY::A, KEY::B, KEY::C, KEY::A, KEY::B],
-        [KEY::B, KEY::C, KEY::A, KEY::B, KEY::C, KEY::A, KEY::B, KEY::C],
-        [KEY::C, KEY::A, KEY::B, KEY::C, KEY::A, KEY::B, KEY::C, KEY::A],
+        [k!(A), k!(B), k!(C), k!(A), k!(B), k!(C), k!(A), k!(B)],
+        [k!(B), k!(C), k!(A), k!(B), k!(C), k!(A), k!(B), k!(C)],
+        [k!(C), k!(A), k!(B), k!(C), k!(A), k!(B), None, None],
     ],
     // Layer 2
     [
-        [KEY::TRNS, KEY::TRNS, KEY::TRNS, KEY::TRNS, KEY::TRNS, KEY::TRNS, KEY::TRNS, KEY::TRNS],
-        [KEY::B, KEY::C, KEY::A, KEY::B, KEY::C, KEY::A, KEY::B, KEY::C],
-        [KEY::C, KEY::A, KEY::B, KEY::C, KEY::A, KEY::B, KEY::C, KEY::A],
+        [k!(A), k!(B), k!(C), k!(A), k!(B), k!(C), k!(A), k!(B)],
+        [k!(B), k!(C), k!(A), k!(B), k!(C), k!(A), k!(B), k!(C)],
+        [None, k!(A), k!(B), k!(C), k!(A), k!(B), k!(C), k!(A)],
     ],
 ];
 
-// -------------------------------------------------------------------------
+// -------- ^^^^^^^^^^^^^^^^^^ --------
 
 
 /*
-// Define the KEY enum with different keyboard keys
-#[derive(Debug, Clone, Copy)]
-enum KEY {
-    A, B, C, TRNS, NULL // ... add more keys as needed
-}
-
-// Define the KEYMAP type, representing a 3D array of keys for each layer, row, and column
-type KEYMAP = [[[KEY; COL_SIZE]; ROW_SIZE]; LAYER_COUNT];
+//TEST debug
 
 // Function to get the key at a specific layer, row, and column
 fn get_key(layer: usize, row: usize, col: usize) -> KEY {
-    KEYMAP_LAYOUT[layer][row][col]
+    if let Some(row_data) = KEYMAP_LAYOUT.get(layer) {
+        if let Some(row) = row_data.get(row) {
+            if let Some(&key) = row.get(col) {
+                return key;
+            }
+        }
+    }
+    None
 }
 
 // Example usage of the keymap
 fn main() {
-    let layer = 1; // Select Layer 1
+    let active_layer = 1; // Select Layer 1 (0-indexed)
     let row = 2;   // Select Row 3 (0-indexed)
-    let col = 3;   // Select Column 4 (0-indexed)
+    let col = 7;   // Select Column 8 (0-indexed)
 
-    let key = get_key(layer, row, col);
-  
-    //debug:
-    //println!("Key pressed: {:?}", key);
+    if let Some(key) = get_key(active_layer, row, col) {
+        println!("Key pressed: {:?}", key);
+    } else {
+        println!("No key at this position.");
+    }
 }
 */
