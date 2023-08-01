39a328dcc014a82ac9b5c9e458a85d8865a5bd32
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-08-01 16:01:02 +0200

    Update keymap_definitions.rs

--- src/keymap_definitions.rs
+++ src/keymap_definitions.rs
@@ -2,18 +2,24 @@
 
 #![no_std]
 
+mod keycodes;
+//TOOD: put KEY in keycodes.rs ?
+//use keycodes::{KEY};
+mod keymap;
+use keymap::{COL_SIZE, ROW_SIZE, LAYER_COUNT};
+
 //TODO:
 
-// Define the KEY enum with different keyboard keys
+// KEY enum
 #[derive(Debug, Clone, Copy)]
 enum KEY {
-    A, B, C, TRNS, MEDIA_VOL_UP, MEDIA_PAUSE, ESC, ENTER, BCKSPACE, DEL, SHIFT, CTRL, ALT,/* ... add more keys as needed */
+    A, B, C, TRNS, MEDIA_VOL_UP, MEDIA_PAUSE, ESC, ENTER, BCKSPACE, DEL, SHIFT, CTRL, ALT, //TOOD: add more keys
 }
 
-// Define the KEYMAP type, representing a 3D array of keys for each layer, row, and column
+// KEYMAP type, representing a 3D array of keys for each column of each row of each layer.
 type KEYMAP = [[[Option<KEY>; COL_SIZE]; ROW_SIZE]; LAYER_COUNT];
 
-// Macro to wrap the keys with Some()
+// Macro to wrap the keys with Some(), used for key!(A) inside the KEYMAP for example
 macro_rules! key {
     ($key:ident) => {
         Some(KEY::$key)
