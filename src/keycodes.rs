f36144a3ee9af02a4fc28ed91129c5510aca13b8
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-08-01 16:02:23 +0200

    Update keycodes.rs

--- src/keycodes.rs
+++ src/keycodes.rs
@@ -1,3 +1,12 @@
 // check https://gist.github.com/MightyPork/6da26e382a7ad91b5496ee55fdc73db2
 // or https://github.com/TeXitoi/keyberon/blob/45b8810e50e87e63adb8629f96778a86affda507/src/key_code.rs
 
+// ?
+mod keymap_definitions;
+use keymap_definitions::KEY;
+// or put this here? :
+// KEY enum
+#[derive(Debug, Clone, Copy)]
+enum KEY {
+    A, B, C, TRNS, MEDIA_VOL_UP, MEDIA_PAUSE, ESC, ENTER, BCKSPACE, DEL, SHIFT, CTRL, ALT, //TODO: add more keys
+}
